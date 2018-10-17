1e40356fd3c9831768db5788ef6b35d640a20dd1
Kent Kurosawa <mots_de_la_magie.s@outlook.jp>
2018-10-17 20:14:37 +0900

    タスク#5テストシーン作成
    ギミック「橋をかける」実装済み

---
+++ project-vr/Assets/Scripts/ControlCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCamera : MonoBehaviour {
+    GameObject targetObject;
+    Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+        targetObject = GameObject.Find("Player");
+        targetPosition = targetObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += targetObject.transform.position - targetPosition;
+        targetPosition = targetObject.transform.position;
+
+        if (Input.GetMouseButton(1))
+        {
+            float mouseInputX = Input.GetAxis("Mouse X");
+            float mouseInputY = -Input.GetAxis("Mouse Y");
+
+            transform.RotateAround(targetPosition, Vector3.up, mouseInputX * Time.deltaTime * 200.0f);
+            transform.RotateAround(targetPosition, transform.right, mouseInputY * Time.deltaTime * 200.0f);
+        }
+	}
+}
---
+++ project-vr/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+    float inputHorizontal, inputVertical;
+    Rigidbody rigidbody;
+
+    [SerializeField]
+    private float jumpPower = 1.0f;
+
+    float moveSpeed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() {
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1));
+
+        Vector3 moveForward = cameraForward * inputVertical + Camera.main.transform.right * inputHorizontal;
+
+        rigidbody.velocity = moveForward * moveSpeed + new Vector3(0, rigidbody.velocity.y, 0);
+
+        if (moveForward != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveForward);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.velocity += new Vector3(0, jumpPower, 0);
+        }
+        rigidbody.velocity += new Vector3(0, Physics.gravity.y * Time.deltaTime, 0);
+    }
+}
