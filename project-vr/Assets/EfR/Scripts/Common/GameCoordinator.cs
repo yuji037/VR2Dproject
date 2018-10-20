eed90e8f45f244736b2521575f2b4e1a82bfd5d5
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2018-10-20 17:19:51 +0900

    Scene類の修正

--- project-vr/Assets/EfR/Scripts/Common/GameCoordinator.cs
+++ project-vr/Assets/EfR/Scripts/Common/GameCoordinator.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GameCoordinator : MonoBehaviour {
 
@@ -10,7 +9,9 @@ public class GameCoordinator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        SceneManager.LoadSceneAsync(firstLoadScene, LoadSceneMode.Additive);
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(
+            firstLoadScene
+            , UnityEngine.SceneManagement.LoadSceneMode.Additive);
 	}
 	
 	// Update is called once per frame
