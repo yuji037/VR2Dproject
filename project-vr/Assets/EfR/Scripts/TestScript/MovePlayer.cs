0ab489e75c123274d32de0b05fd4191c374370fc
ganigani512 <ganiganick512@gmail.com>
2018-10-22 17:34:38 +0900

    ネットワーク系のテストの途中（豊田用）

--- project-vr/Assets/EfR/Scripts/TestScript/MovePlayer.cs
+++ project-vr/Assets/EfR/Scripts/TestScript/MovePlayer.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MovePlayer : MonoBehaviour {
+    NetworkView networkview;
+
     float inputHorizontal, inputVertical;
     Rigidbody rigidbody;
 
@@ -14,7 +16,8 @@ public class MovePlayer : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         rigidbody = GetComponent<Rigidbody>();
-	}
+        networkview = GetComponent<NetworkView>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -23,21 +26,25 @@ public class MovePlayer : MonoBehaviour {
     }
 
     void FixedUpdate() {
-        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1));
-
-        Vector3 moveForward = cameraForward * inputVertical + Camera.main.transform.right * inputHorizontal;
-
-        rigidbody.velocity = moveForward * moveSpeed + new Vector3(0, rigidbody.velocity.y, 0);
-
-        if (moveForward != Vector3.zero)
+        if (networkview.isMine)
         {
-            transform.rotation = Quaternion.LookRotation(moveForward);
-        }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rigidbody.velocity += new Vector3(0, jumpPower, 0);
+            Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1));
+
+            Vector3 moveForward = cameraForward * inputVertical + Camera.main.transform.right * inputHorizontal;
+
+            rigidbody.velocity = moveForward * moveSpeed + new Vector3(0, rigidbody.velocity.y, 0);
+
+            if (moveForward != Vector3.zero)
+            {
+                transform.rotation = Quaternion.LookRotation(moveForward);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rigidbody.velocity += new Vector3(0, jumpPower, 0);
+            }
+            rigidbody.velocity += new Vector3(0, Physics.gravity.y * Time.deltaTime, 0);
         }
-        rigidbody.velocity += new Vector3(0, Physics.gravity.y * Time.deltaTime, 0);
     }
 }
