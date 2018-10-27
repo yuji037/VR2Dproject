c26e1aa191fc5e99fb37c7282efd4c62d5c0b414
SoraneGabriel <aaaa@documo.ne.jp>
2018-10-27 12:54:24 +0900

    Kent Kurosawa Task#21 テスト用 Oculus Touchの入力 Rayが取れず。

--- project-vr/Assets/EfR/Scenes/Kurosawa/OculusInput.cs
+++ project-vr/Assets/EfR/Scenes/Kurosawa/OculusInput.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class OculusInput : MonoBehaviour {
-    [SerializeField]
-    GameObject oculus;
+
+    private OVRRaycaster ray;
+    private OVRCameraRig rig;
+    private RaycastHit hit;
 
     private void Update()
     {
-        if (OVRInput.GetDown(OVRInput.RawTouch.LIndexTrigger))
+        if (OVRInput.GetDown(OVRInput.Button.One))
         {
-            Debug.Log("OVRInput.RawTouch.LIndexTrigger");
+            Debug.Log("A");
+        }
+
+        if (OVRInput.Get(OVRInput.Button.Four))
+        {
+            Debug.Log("Y");
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            Debug.Log("OVRInput.RawButton.LIndexTrigger");
+        }
+
+        if (OVRInput.Get(OVRInput.RawTouch.LIndexTrigger))
+        {
+          //  Debug.Log("OVRInput.RawTouch.LIndexTrigger");
+        }
+
+        if (OVRInput.Get(OVRInput.RawNearTouch.LIndexTrigger))
+        {
+            if (Physics.Raycast(rig.leftHandAnchor.transform.position, ray.transform.forward, out hit, Mathf.Infinity))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+            }
         }
     }
 }
--- project-vr/Assets/EfR/Scenes/Kurosawa/WireAction.cs
+++ project-vr/Assets/EfR/Scenes/Kurosawa/WireAction.cs
@@ -7,8 +7,9 @@ public class WireAction : MonoBehaviour
 {
     [SerializeField]
     float wireAct;
+    [SerializeField]
+    GameObject player;
 
-    private GameObject player = null;
     private Rigidbody playersRigidbody = null;
     private Vector3 endWire;
     private float wireLength = 0.0f;
@@ -17,7 +18,6 @@ public class WireAction : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
         if (player == null) { Debug.LogError("Don't Found Player"); }
 
         playersRigidbody = player.GetComponent<Rigidbody>();
