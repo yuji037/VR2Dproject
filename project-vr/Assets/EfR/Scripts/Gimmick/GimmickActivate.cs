487ecc49613532923fe58c32539c99604c4a5e09
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2018-10-29 19:56:57 +0900

    タスク#18実装

---
+++ project-vr/Assets/EfR/Scripts/Gimmick/GimmickActivate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GimmickActivate : GimmickBase {
+
+    [SerializeField]
+    bool isDefaultActive = false;
+
+    private void Start()
+    {
+        gameObject.SetActive(isDefaultActive);
+    }
+}
