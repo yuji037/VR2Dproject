439ed8a280775b6778f6b4e5352c8fbdc0150f64
Kent Kurosawa <mots_de_la_magie.s@outlook.jp>
2018-11-07 23:34:35 +0900

    ブロックを押すスクリプトをコミット
    Scripts/Character配下にとりあえず置いておきました。

---
+++ project-vr/Assets/EfR/Scripts/Character/Test_SphereCast.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_SphereCast : MonoBehaviour {
+
+    private RaycastHit hit;
+    private bool isHit = false;
+
+    [SerializeField]
+    LayerMask layerMask;
+
+    [SerializeField]
+    float radius;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        Vector3 distance = other.transform.position - transform.position;
+        float x = Mathf.Abs(distance.x);
+        float z = Mathf.Abs(distance.z);
+
+        if (x > z) { distance.z = 0; }
+        else { distance.x = 0; }
+        distance.y = 0;
+
+        Vector3.Normalize(distance);
+        
+        // 押した先に当たり判定がある物体が存在するか確認しなければ押せるようにする
+
+
+        other.transform.Translate(distance * Time.deltaTime);
+    }
+}
