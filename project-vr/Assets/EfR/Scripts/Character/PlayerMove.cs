ccbe8688c3b7965ddfd08a0f835822aea7ec6bbe
umei <umeryosan@gmail.com>
2018-11-10 20:33:05 +0900

    PlayerMoveのPlayerMoveSettingsのInspectorが外れることがあったので修正

--- project-vr/Assets/EfR/Scripts/Character/PlayerMove.cs
+++ project-vr/Assets/EfR/Scripts/Character/PlayerMove.cs
@@ -3,20 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[CreateAssetMenu(fileName = "PlayerMoveSettings", menuName = "Create PlayerMoveSettings", order = 0)]
-public class PlayerMoveSettings : ScriptableObject {
 
-    public bool canJump;
-    public float jumpPower;
-    public float limitSpeed;
-    public float groundMoveAccel;
-    public float airMoveAccel;
-    public float gravity;
-    public float airVerticalResistance;
-    public float airResistance;
-    public float groundFriction;
-    public float distanceToGround;
-}
 
 
 public class PlayerMove : MonoBehaviour {
