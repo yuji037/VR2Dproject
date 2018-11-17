d1faf1dcd6f9a0322039dd59ec4a60a15d10df39
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2018-11-17 11:13:05 +0900

    ネットワーク対応カメラ修正

---
+++ project-vr/Assets/EfR/Scripts/Common/VRObjectManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum VRDeviceType {
+    NO_DEVICE,
+    OCULUS,
+    HTC_VIVE,
+}
+
+public class VRObjectManager : SingletonMonoBehaviour<VRObjectManager> {
+
+
+    [SerializeField]
+    GameObject[] m_prefVRCams;
+
+    public VRDeviceType DeviceType { get; private set; }
+
+    GameObject m_oCam;
+
+    public void SetDeviceType(VRDeviceType deviceType)
+    {
+        this.DeviceType = deviceType;
+    }
+    
+    public void SpawnVRCamObject()
+    {
+        var trParent = GameObject.Find("VRCamParent").transform;
+        m_oCam = Instantiate(m_prefVRCams[(int)DeviceType]);
+
+        m_oCam.transform.parent = trParent;
+    }
+
+    // プレイヤースポーン後に呼ぶ
+    public void InitVRCamObject()
+    {
+        var oPlayer = PlayerManager.LocalPlayer;
+        var plMove = oPlayer.GetComponent<PlayerMove>();
+
+        // カメラ生成
+        var camTr = GameObject.Find("CamPos" + plMove.moveType.ToString()).transform;
+        m_oCam.transform.position = camTr.position;
+        m_oCam.transform.rotation = camTr.rotation;
+
+        //if ( moveType == MoveType._2D )
+        //{
+        //    // 2Dの場合のみ、カメラはZ軸の-方向から見る配置にする
+        //    var distanceVec = this.transform.position - camTr.position;
+        //    camRig.transform.position = this.transform.position - new Vector3(0, 0, distanceVec.magnitude);
+        //    camRig.transform.rotation = Quaternion.identity;
+        //}
+        var cc = m_oCam.GetComponent<ControlCameraOVRRig>();
+        cc.Init();
+
+    }
+}
---
+++ project-vr/Assets/EfR/Scripts/Utility/PushableObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushableObject : MonoBehaviour {
+
+    [SerializeField]
+    LayerMask triggerLayerMask = -1;
+    [SerializeField]
+    float pushStrength = 0.5f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        var otherLayerMask = 1 << other.gameObject.layer;
+        // 選択したレイヤー以外のオブジェクトに当たっても押されない
+        if ( (otherLayerMask & triggerLayerMask.value) == 0 ) return;
+
+        Vector3 distance = transform.position - other.transform.position;
+        float x = Mathf.Abs(distance.x);
+        float z = Mathf.Abs(distance.z);
+
+        if ( x > z ) { distance.z = 0; }
+        else { distance.x = 0; }
+        distance.y = 0;
+
+        distance /= distance.magnitude;
+
+        //Debug.DrawLine
+
+        // 押される方向に当たり判定がある物体があったら押されない
+        if ( Physics.BoxCast(transform.position + distance, Vector3.one * 0.4f, distance, Quaternion.identity, 0.1f) )
+            return;
+
+        transform.Translate(distance * pushStrength * Time.deltaTime);
+    }
+}
