7eec8af068af52c2b3f3b0022579c6149b8a3b2c
umeigon <umeryosan@gmail.com>
2018-12-03 22:07:58 +0900

    レーザーを飛ばすベースクラスRazerPointerBaseを作成、mirrorRayShooterをリネーム

---
+++ project-vr/Assets/EfR/Scripts/Pointer/RazerPointerBase.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RazerPointerBase : MonoBehaviour
+{
+    [SerializeField]
+    Transform shooter;
+
+    LineRenderer lineRenderer;
+
+    List<Vector3> lineRenderPositions = new List<Vector3>();
+
+    [SerializeField]
+    LayerMask layerMask;
+
+    //このRayを飛ばしているGimmickBaseを持ったコライダー
+    [SerializeField]
+    protected Collider ownerCollider;
+
+    private void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnFlameStart();
+        lineRenderPositions.Clear();
+        ShootRay(shooter.position, shooter.forward);
+        ApplyLineRenderPositions();
+    }
+    //マックスカウントまで反射するrayを飛ばし続ける再起関数
+    protected void ShootRay(Vector3 origin, Vector3 direction)
+    {
+        SetPosition(origin);
+        RaycastHit hit;
+        Ray ray = new Ray(origin, direction);
+        bool isHit = Physics.Raycast(ray, out hit, 1000f, layerMask);
+        if (isHit)
+        {
+            HitAction(hit,origin,direction);
+        }
+        else
+        {
+            NoHitAction(origin,direction);
+        }
+    }
+    protected virtual void OnFlameStart()
+    {
+    }
+    protected virtual void HitAction(RaycastHit hit, Vector3 origin, Vector3 direction)
+    {
+    }
+    protected virtual void NoHitAction(Vector3 origin, Vector3 direction)
+    {
+        SetPosition(origin + direction * 10);
+    }
+    protected void SetPosition(Vector3 position)
+    {
+        lineRenderPositions.Add(position);
+    }
+    void ApplyLineRenderPositions()
+    {
+        lineRenderer.positionCount = lineRenderPositions.Count;
+        for (int i = 0; i < lineRenderer.positionCount; i++)
+        {
+            lineRenderer.SetPosition(i, lineRenderPositions[i]);
+        }
+    }
+}
---
+++ project-vr/Assets/EfR/Scripts/Pointer/RazerPointerMirror.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RazerPointerMirror : RazerPointerBase
+{
+    [SerializeField]
+    int maxReflection = 30;
+    int reflectionCount = 0;
+
+    protected override void HitAction(RaycastHit hit, Vector3 origin, Vector3 direction)
+    {
+        reflectionCount++;
+        if (hit.collider.GetComponent<Mirror>())
+        {
+
+            //発射地点から衝突点までのベクトル
+            var rayVec = hit.point - origin;
+            //法線の長さ
+            var a = Vector3.Dot(-rayVec, hit.normal);
+            //Hitした平面の平行ベクトル
+            var hitHorizontalVec = rayVec + hit.normal * a;
+            var reflectionPoint = origin + hitHorizontalVec * 2;
+            var reflectionDir = (reflectionPoint - hit.point).normalized;
+            ShootRay(hit.point, reflectionDir);
+        }
+        else
+        {
+            SetPosition(hit.point);
+            GimmickBase gimmick=null;
+            if ((gimmick = hit.collider.GetComponent<GimmickBase>()) != null
+                && (PlayerManager.LocalPlayer.GetComponent<PlayerMove>().moveType != PlayerMove.MoveType._2D))
+            {
+                gimmick.OnPointerHit(ownerCollider);
+            }
+        }
+    }
+    protected override void OnFlameStart()
+    {
+        reflectionCount=0;
+    }
+}
