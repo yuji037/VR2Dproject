723312b74ee3f902be09dde3a09d09389f445188
Kent Kurosawa <mots_de_la_magie.s@outlook.jp>
2018-12-06 03:19:38 +0900

    LocalPlayerの状態を見て、マテリアルの状態を変えるクラスを追加

---
+++ project-vr/Assets/EfR/Scripts/Utility/ChangeMaterial.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ChangeMaterial : NetworkBehaviour {
+
+    private enum Elements
+    {
+        _VR,
+        _2D,
+    }
+
+    private new MeshRenderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        if (renderer == null) { Debug.LogError("Can't get MeshRenderer component."); }
+
+        var thisObject = renderer.gameObject;
+        if( !MaterialsManager.GetInstance().Add(thisObject) ) { Debug.LogError("Can't add list this object"); }
+    }
+
+    public void Change(PlayerMove.MoveType _moveType)
+    {
+        if (renderer.materials.Length == 1) { return; }
+
+        switch ( _moveType )
+        {
+            case PlayerMove.MoveType.FPS:
+            case PlayerMove.MoveType.TPS:
+                renderer.material = renderer.materials[(int)Elements._VR];
+                break;
+
+            case PlayerMove.MoveType._2D:
+                renderer.material = renderer.materials[(int)Elements._2D];
+                break;
+        }
+    }
+}
---
+++ project-vr/Assets/EfR/Scripts/Utility/MaterialsManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialsManager : SingletonMonoBehaviour<MaterialsManager>
+{
+    private List<GameObject> objects;
+
+    // オブジェクトが自身をListに登録する関数
+    public bool Add(GameObject _object)
+    {
+        objects.Add(_object);
+        return true;
+    }
+
+    // リストに登録されているオブジェクトのマテリアルを書き換える
+    public void Change()
+    {
+        // このPCのPlayerが今どの状態か取得
+        PlayerMove.MoveType localPlayersMoveType = PlayerManager.LocalPlayer.GetComponent<PlayerMove>().moveType;
+
+        foreach (GameObject _object in objects)
+        {
+            _object.GetComponent<ChangeMaterial>().Change(localPlayersMoveType);
+        }
+    }
+}
