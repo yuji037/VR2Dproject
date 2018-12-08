2b640e12d8a7824a9196ae84bac9aff2d8dfa719
umeigon <umeryosan@gmail.com>
2018-12-08 13:26:05 +0900

    床prefabの当たり判定がズレていたのを修正。Hierarchy右クリックからScreen生成を追加

--- project-vr/Assets/EfR/Editor/GimmickObjectGenerator.cs
+++ project-vr/Assets/EfR/Editor/GimmickObjectGenerator.cs
@@ -5,57 +5,76 @@ using UnityEngine;
 using UnityEditor;
 public class GimmickObjectGenerator : Editor
 {
-    static string assetPath = "Assets/EfR/Prefabs/Gimmick/";
-    static GameObject InstantiateGimmick(string prefabName)
+    static string gimmickPath = "Assets/EfR/Prefabs/Gimmick/";
+    static string screenPath = "Assets/EfR/Prefabs/Screen/";
+
+    static GameObject InstantiateGimmick(string prefabPath)
     {
-        GameObject prefabObject = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath + prefabName);
+        GameObject prefabObject = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
         var prefabCopy = PrefabUtility.InstantiatePrefab(prefabObject) as GameObject;
-        if (Selection.activeGameObject!=null)
+        if (Selection.activeGameObject != null)
         {
             prefabCopy.transform.parent = Selection.activeGameObject.transform;
         }
         Selection.activeGameObject = prefabCopy;
-            prefabCopy.transform.localPosition = Vector3.zero;
-        Undo.RegisterCreatedObjectUndo(prefabCopy, "Create" + prefabName);
+        prefabCopy.transform.localPosition = Vector3.zero;
+        Undo.RegisterCreatedObjectUndo(prefabCopy, "Create" + prefabCopy.name);
         return prefabCopy;
     }
 
-    
-    static void InstantiateAndSetGimmickID(string prefabName, int minNeedID = 1000)
+
+    static void InstantiateAndSetGimmickID(string prefabPath, int minNeedID = 1000)
     {
-        var obj = InstantiateGimmick(prefabName);
+        var obj = InstantiateGimmick(prefabPath);
         var gb = obj.GetComponent<GimmickBase>();
-        Undo.RecordObject(gb, "RegistGimmickID" + prefabName);
+        Undo.RecordObject(gb, "RegistGimmickID" + prefabPath);
         gb.SetGimmickID(GimmickIDManager.GetNotOverLapGimmickID(minNeedID));
     }
-    [MenuItem("GameObject/EFR_Gimmicks/TransmissionLine", priority = 21)]
+    [MenuItem("GameObject/EFR_Gimmick/TransmissionLine", priority = 21)]
     static void CreateLine()
     {
-        InstantiateGimmick("TransmissionLine.prefab");
+        InstantiateGimmick(gimmickPath + "TransmissionLine.prefab");
     }
 
-    [MenuItem("GameObject/EFR_Gimmicks/GimmickCube", priority = 22)]
+    [MenuItem("GameObject/EFR_Gimmick/GimmickCube", priority = 22)]
     static void CreateGimmickCube()
     {
-        InstantiateGimmick("GimmickCube.prefab");
+        InstantiateGimmick(gimmickPath + "GimmickCube.prefab");
     }
 
-    [MenuItem("GameObject/EFR_Gimmicks/GimmickSwitch", priority = 23)]
+    [MenuItem("GameObject/EFR_Gimmick/GimmickSwitch", priority = 23)]
     static void CreateGimmickSwitch()
     {
-        InstantiateAndSetGimmickID("GimmickSwitch.prefab");
+        InstantiateAndSetGimmickID(gimmickPath + "GimmickSwitch.prefab");
     }
 
-    [MenuItem("GameObject/EFR_Gimmicks/GimmickDoor", priority = 24)]
+    [MenuItem("GameObject/EFR_Gimmick/GimmickDoor", priority = 24)]
     static void CreateGimmickDoor()
     {
-        InstantiateAndSetGimmickID("GimmickDoor.prefab");
+        InstantiateAndSetGimmickID(gimmickPath + "GimmickDoor.prefab");
     }
 
-    [MenuItem("GameObject/EFR_Gimmicks/GimmickWorldFlipFlop", priority = 25)]
+    [MenuItem("GameObject/EFR_Gimmick/GimmickWorldFlipFlop", priority = 25)]
     static void CreateGimmickWorldFlipFlop()
     {
-        InstantiateAndSetGimmickID("GimmickWorldFlipFlop.prefab");
+        InstantiateAndSetGimmickID(gimmickPath + "GimmickWorldFlipFlop.prefab");
+    }
+    [MenuItem("GameObject/EFR_Screen/Normal", priority = 26)]
+    static void CreateNoramlFloorScreen()
+    {
+        InstantiateGimmick(screenPath + "NormalFloorScreen.prefab");
+    }
+
+    [MenuItem("GameObject/EFR_Screen/LongX", priority = 27)]
+    static void CreateLongXFloorScreen()
+    {
+        InstantiateGimmick(screenPath + "LongXFloorScreen.prefab");
+    }
+
+    [MenuItem("GameObject/EFR_Screen/LongY", priority = 28)]
+    static void CreateLongYFloorScreen()
+    {
+        InstantiateGimmick(screenPath + "LongYFloorScreen.prefab");
     }
 
 }
