83f42635919c0b5603e1ffc515e7e97bc199342d
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2018-12-25 20:20:45 +0900

    SoundManager仮作成

---
+++ project-vr/Assets/EfR/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : SingletonMonoBehaviour<SoundManager> {
+
+	Dictionary<string, AudioClip> m_AudioClips = new Dictionary<string, AudioClip>();
+
+	[SerializeField, Header("音を持たせるオブジェクト")]
+	GameObject m_prefSound;
+
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		var audioClips = Resources.LoadAll<AudioClip>( "BGM" );
+		foreach(var ac in audioClips )
+		{
+			m_AudioClips[ac.name] = ac;
+			Debug.Log(ac.name);
+		}
+	}
+
+	public void Play(string name, Vector3? position = null, bool isLoop = false, bool playIn3DVolume = true)
+	{
+		if( position == null )
+		{
+			position = Vector3.zero;
+		}
+		var obj = Instantiate( m_prefSound, transform.position, Quaternion.identity, transform );
+		var soundPlayIns = obj.GetComponent<SoundPlayIns>();
+		soundPlayIns.Init();
+
+		var audioSource = soundPlayIns.m_AudioSource;
+		audioSource.clip = m_AudioClips[name];
+		audioSource.loop = isLoop;
+
+		// ループしないなら自前で停止
+		if ( !isLoop )
+		{
+			StartCoroutine( DestroyOnClipEndCoroutine( soundPlayIns ) );
+		}
+
+		if ( !playIn3DVolume )
+		{
+			audioSource.spatialBlend = 0f;
+		}
+
+		audioSource.Play();
+	}
+
+	public void PlayOnAttachedTransform(string name, Transform attachPoint, bool isLoop = false)
+	{
+		var obj = Instantiate( m_prefSound, attachPoint.position, attachPoint.rotation, attachPoint );
+		var soundPlayIns = obj.GetComponent<SoundPlayIns>();
+		soundPlayIns.Init();
+		soundPlayIns.AttachTarget( attachPoint );
+
+		var audioSource = soundPlayIns.m_AudioSource;
+		audioSource.clip = m_AudioClips[name];
+		audioSource.loop = isLoop;
+
+		// ループしないなら自前で停止
+		if ( !isLoop )
+		{
+			StartCoroutine( DestroyOnClipEndCoroutine( soundPlayIns ) );
+		}
+
+		audioSource.Play();
+	}
+
+	IEnumerator DestroyOnClipEndCoroutine(SoundPlayIns soundPlayIns)
+	{
+		float clipLength = soundPlayIns.m_AudioSource.clip.length;
+
+		yield return new WaitForSeconds( clipLength + 0.5f );
+
+		Destroy( soundPlayIns.gameObject );
+	}
+
+	public void StopAndDestroy(SoundPlayIns soundPlayIns)
+	{
+
+	}
+
+	public void FadeOut()
+	{
+		//StartCoroutine(FadeOutCoroutine());
+	}
+
+}
+
---
+++ project-vr/Assets/EfR/Scripts/Sound/SoundPlayIns.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SoundPlayIns : MonoBehaviour{
+
+	public AudioSource m_AudioSource;
+
+	Transform m_trAttachedTarget = null;
+
+	public void Init()
+	{
+		m_AudioSource = GetComponent<AudioSource>();
+	}
+
+	private void Update()
+	{
+		if ( m_trAttachedTarget )
+		{
+			transform.position = m_trAttachedTarget.position;
+		}
+	}
+
+	public void ChangeVolume(float afterVolume, float changeDuration = 1.0f)
+	{
+		if ( afterVolume == 0f )
+		{
+			Debug.LogWarning( "SoundOneIns.ChangeVolumeによってVolume→0になりますが破棄されません" );
+		}
+		StartCoroutine( ChangeVolumeCoroutine( afterVolume, changeDuration ) );
+	}
+
+	public void FadeoutAndDestroy(float duration = 1.0f)
+	{
+		StartCoroutine( ChangeVolumeCoroutine( 0f, duration ) );
+	}
+
+	IEnumerator ChangeVolumeCoroutine(float afterVolume, float duration, Action onEnd = null)
+	{
+		float beforeVolume = m_AudioSource.volume;
+
+		for ( float t = 0; t < duration; t += Time.unscaledDeltaTime )
+		{
+			float transRate = t / duration;
+
+			m_AudioSource.volume =
+				beforeVolume * ( 1 - transRate ) +
+				afterVolume * transRate;
+
+			yield return null;
+		}
+
+		m_AudioSource.volume = afterVolume;
+
+		if ( onEnd != null)
+		{
+			onEnd();
+		}
+	}
+
+	// 音の再生位置をオブジェクトに追従させる
+	public void AttachTarget(Transform targetTransform)
+	{
+
+	}
+
+}
