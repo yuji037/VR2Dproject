5825f50e66446241db2a804e5538501c56473ae3
umei <umeryosan@gmail.com>
2019-01-20 01:29:18 +0900

    生成床のコライダーサイズを変更するスクリプト追加。

--- project-vr/Assets/EfR/Scripts/Gimmick/GimmickFloor.cs
+++ project-vr/Assets/EfR/Scripts/Gimmick/GimmickFloor.cs
@@ -11,9 +11,17 @@ public enum FloorForm
 public class GimmickFloor : GimmickBase
 {
     public FloorForm floorForm;
+    BoxColliderInfo fBox;
+    Vector3 defaultBoxSize;
+    Vector3 defaultBoxCenter;
+    BoxCollider boxCollider;
     private new void Awake()
     {
         base.Awake();
+        fBox = GetComponent<BoxColliderInfo>();
+        boxCollider = GetComponent<BoxCollider>();
+        defaultBoxSize = boxCollider.size;
+        defaultBoxCenter = boxCollider.center;
     }
     [ClientRpc]
     public void RpcInit(NetworkIdentity playerNetID)
@@ -22,5 +30,69 @@ public class GimmickFloor : GimmickBase
         {
             GimmickFloorSpawner.GetInstance().RegisterFloor(this);
         }
+    }
+
+    public void CorrectCollider(PointerHitScreen pointerHitScreen)
+    {
+        Vector3 boxSize = defaultBoxSize;
+        Vector3 boxPos = defaultBoxCenter;
+        var sBox = pointerHitScreen.GetComponent<BoxColliderInfo>();
+        var pivot = toYzero(pointerHitScreen.transform.position);
+        var rot = pointerHitScreen.transform.rotation;
+
+        var rSub = (
+                Vector3.Distance(toYzero(fBox.RightPoint), pivot) -
+                Vector3.Distance(toYzero(sBox.RightPoint), pivot)
+                ) / transform.lossyScale.x;
+        if (rSub > 0)
+        {
+            boxSize.x = defaultBoxSize.x - rSub;
+            boxPos = new Vector3(-rSub / 2, boxPos.y, boxPos.z);
+        }
+
+        var lSub = (
+                Vector3.Distance(toYzero(fBox.LeftPoint), pivot) -
+                Vector3.Distance(toYzero(sBox.LeftPoint), pivot)
+                ) / transform.lossyScale.x;
+        if (lSub > 0)
+        {
+            boxSize.x = defaultBoxSize.x - lSub;
+            boxPos = new Vector3(lSub / 2, boxPos.y, boxPos.z);
+        }
+
+        pivot = toXzero(pointerHitScreen.transform.position);
+
+        var tSub = (
+               Vector3.Distance(toXzero(fBox.TopPoint), pivot) -
+               Vector3.Distance(toXzero(sBox.TopPoint), pivot)
+               ) / transform.lossyScale.y;
+        if (tSub > 0)
+        {
+            boxSize.y = defaultBoxSize.y - tSub;
+            boxPos = new Vector3(boxPos.x, -tSub / 2, boxPos.z);
+        }
+
+        var bSub = (
+               Vector3.Distance(toXzero(fBox.BottomPoint), pivot) -
+               Vector3.Distance(toXzero(sBox.BottomPoint), pivot)
+               ) / transform.lossyScale.y;
+        if (bSub > 0)
+        {
+            boxSize.y = defaultBoxSize.y - bSub;
+            boxPos = new Vector3(boxPos.x, bSub / 2, boxPos.z);
+        }
+
+        boxCollider.size = boxSize;
+        boxCollider.center = boxPos;
+    }
+    Vector3 toYzero(Vector3 vector)
+    {
+        vector.y = 0;
+        return vector;
+    }
+    Vector3 toXzero(Vector3 vector)
+    {
+        vector.x = 0;
+        return vector;
     }
 }
---
+++ project-vr/Assets/EfR/Scripts/Pointer/BoxColliderInfo.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxColliderInfo : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        float correct = 0.1f;
+        Gizmos.color = Color.black;
+
+
+
+        var upper = TopSide;
+        var right = RightSide;
+        var left = LeftSide;
+        var lower = BottomSide;
+        Gizmos.DrawLine(new Vector3(right, upper, transform.position.z - correct), new Vector3(left, upper, transform.position.z - correct));
+
+        Gizmos.DrawLine(new Vector3(right, lower, transform.position.z - correct), new Vector3(left, lower, transform.position.z - correct));
+
+        Gizmos.DrawLine(new Vector3(right, upper, transform.position.z - correct), new Vector3(right, lower, transform.position.z - correct));
+
+        Gizmos.DrawLine(new Vector3(left, lower, transform.position.z - correct), new Vector3(left, upper, transform.position.z - correct));
+
+
+    }
+    public float HalfWidth
+    {
+        get { return transform.lossyScale.x * 0.5f; }
+    }
+    public float HalfHeight
+    {
+        get { return transform.lossyScale.y * 0.5f; }
+    }
+    public float BottomSide
+    {
+        get
+        {
+            return transform.position.y - transform.lossyScale.y / 2;
+        }
+    }
+    public float TopSide
+    {
+        get
+        {
+            return transform.position.y + transform.lossyScale.y / 2;
+        }
+    }
+    public float RightSide
+    {
+        get
+        {
+            return transform.position.x + transform.lossyScale.x / 2;
+        }
+    }
+    public float LeftSide
+    {
+        get
+        {
+            return transform.position.x - transform.lossyScale.x / 2;
+        }
+    }
+    public Vector3 RightPoint
+    {
+        get
+        {
+            return transform.position + transform.rotation*new Vector3(HalfWidth,0,0);
+        }
+    }
+
+    public Vector3 LeftPoint
+    {
+        get
+        {
+            return transform.position + transform.rotation * new Vector3(-HalfWidth,0, 0);
+        }
+    }
+
+    public Vector3 TopPoint
+    {
+        get
+        {
+            return transform.position + transform.rotation * new Vector3(0, HalfHeight, 0);
+        }
+    }
+
+    public Vector3 BottomPoint
+    {
+        get
+        {
+            return transform.position + transform.rotation* new Vector3(0,-HalfHeight,  0);
+        }
+    }
+
+    Vector3 RightUp
+    {
+        get
+        {
+            return transform.position + new Vector3(HalfWidth, HalfHeight);
+        }
+    }
+    Vector3 LeftUp
+    {
+        get
+        {
+            return transform.position + new Vector3(-HalfWidth, HalfHeight);
+        }
+    }
+    Vector3 RightDown
+    {
+        get
+        {
+            return transform.position + new Vector3(HalfWidth, -HalfHeight);
+        }
+    }
+    Vector3 LeftDown
+    {
+        get
+        {
+            return transform.position + new Vector3(-HalfWidth, -HalfHeight);
+        }
+    }
+
+}
