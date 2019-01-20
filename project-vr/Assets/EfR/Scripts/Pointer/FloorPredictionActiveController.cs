160d842daf11fd2637ba722eb0e27f17bbed8928
umeigon <umeryosan@gmail.com>
2019-01-21 02:48:54 +0900

    床生成の予兆範囲と生成床のコライダーを動的変更する機能追加

---
+++ project-vr/Assets/EfR/Scripts/Pointer/FloorPredictionActiveController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorPredictionActiveController: MonoBehaviour
+{
+  
+    [SerializeField]
+    GameObject[] PredictionFloorsPrefab;
+
+    FloorPredictionView[] floorPredictions; 
+  private void Start()
+    {
+        floorPredictions = new FloorPredictionView[PredictionFloorsPrefab.Length];
+        for (int i = 0; i < PredictionFloorsPrefab.Length; i++)
+        {
+            floorPredictions[i] = Instantiate(PredictionFloorsPrefab[i]).GetComponent<FloorPredictionView>();
+        }
+    }
+    public void SetView(Vector3 hitPos, PointerHitScreen pointerHitScreen)
+    {
+        FloorPredictionView activePredictionFloor=null;
+
+        var floorForm = pointerHitScreen.GetFloorForm;
+
+        for (int i=0;i< floorPredictions.Length;i++)
+        {
+            var active = (i == (int)floorForm);
+            floorPredictions[i].gameObject.SetActive(active);
+            if (active) activePredictionFloor = floorPredictions[i];
+        }
+        activePredictionFloor.ActiveView(hitPos,pointerHitScreen);
+
+    }
+
+    public void AllInactive()
+    {
+        foreach (var i in floorPredictions)
+        {
+            i.gameObject.SetActive(false);
+        }
+    }
+}
