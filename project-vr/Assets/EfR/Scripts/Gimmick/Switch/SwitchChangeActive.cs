e8a1d8239a6c44ba987fe7944e764f6d749379c1
umei <umeryosan@gmail.com>
2019-02-15 13:58:49 +0900

    1ｐのデフォルト進行方向を右に変更し、windギミックのアクティブをスイッチで切り替えるように変更

---
+++ project-vr/Assets/EfR/Scripts/Gimmick/Switch/SwitchChangeActive.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SwitchChangeActive : SwitchActionBase {
+
+    [SerializeField]
+    GameObject activateTarget;
+
+    [SerializeField]
+    bool targetDefaultActive;
+
+    private void Start()
+    {
+        activateTarget.SetActive(targetDefaultActive);
+    }
+
+    [Command]
+    void CmdSetActive(bool active)
+    {
+        RpcSetActive(active);
+    }
+
+    [ClientRpc]
+    void RpcSetActive(bool active)
+    {
+        activateTarget.SetActive(active);
+    }
+
+    public override void OnAction()
+    {
+        activateTarget.SetActive(!targetDefaultActive);
+    }
+
+    public override void OffAction()
+    {
+        activateTarget.SetActive(targetDefaultActive);
+    }
+    
+}
