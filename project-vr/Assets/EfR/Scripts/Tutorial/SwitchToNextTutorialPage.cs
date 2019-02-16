44bca7b2d968e7bcad59dd900a6b66c6ce7b7df6
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2019-02-16 18:37:23 +0900

    チュートリアル新条件作成

---
+++ project-vr/Assets/EfR/Scripts/Tutorial/SwitchToNextTutorialPage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchToNextTutorialPage : MonoBehaviour {
+
+	[SerializeField]
+	TutorialObject nextTutorialObject;
+
+	// Update is called once per frame
+	void Update () {
+		if( OVRInput.GetDown(OVRInput.Button.One) )
+		{
+			Destroy(gameObject);
+			if ( nextTutorialObject )
+			{
+				var obj = Instantiate(nextTutorialObject.gameObject);
+				var tutorialObjectIns = obj.GetComponent<TutorialObject>();
+				tutorialObjectIns.SetParent();
+				tutorialObjectIns.Init();
+				tutorialObjectIns.transform.localPosition = Vector3.zero;
+				tutorialObjectIns.transform.localRotation = Quaternion.identity;
+			}
+			else
+				PlayerManager.LocalPlayer.GetComponent<PlayerMove>().canMove = true;
+		}
+	}
+}
