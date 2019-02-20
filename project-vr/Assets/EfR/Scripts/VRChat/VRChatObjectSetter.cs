2abea9f684ed26eb3aeb995cca0a4c65d277b398
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2019-02-20 18:02:44 +0900

    VRチャット修正

--- project-vr/Assets/EfR/Scripts/VRChat/VRChatObjectSetter.cs
+++ project-vr/Assets/EfR/Scripts/VRChat/VRChatObjectSetter.cs
@@ -21,41 +21,57 @@ public class VRChatObjectSetter : NetworkBehaviour {
 
 	public void Start()
 	{
-		// 0,1
-		int playerNum = PlayerManager.GetPlayerNumber() + 1;
+        StartCoroutine(SetIKCorotuine());
+	}
 
-		// 1,2
-		int[] nums = new int[] { 1, 2 };
-		if ( !isLocalPlayer )
-			playerNum = nums.Where(num => num != playerNum).First();
+    void SetIKTarget()
+    {
 
-		var vrIK = GameObject.Find("VRChatCharaPos" + ( playerNum)).
-			GetComponentInChildren<VRIK>();
-		Debug.Log("playerNum : " + playerNum + " Init");
+        // 0,1
+        int playerNum = PlayerManager.GetPlayerNumber() + 1;
+
+        // 1,2
+        int[] nums = new int[] { 1, 2 };
+        if (!isLocalPlayer)
+            playerNum = nums.Where(num => num != playerNum).First();
+
+        transform.parent = GameObject.Find("VRChatCharaPos" + (playerNum)).FindFirstChildByName("VRChatChara").transform;
+
+
+        var vrIK = GameObject.Find("VRChatCharaPos" + (playerNum)).
+            GetComponentInChildren<VRIK>();
+        Debug.Log("playerNum : " + playerNum + " Init");
 
         var targetObj = gameObject.FindFirstChildByName("IKTarget");
 
-		switch ( setObjectType )
-		{
-			case TrackType.RightHand:
-				vrIK.solver.rightArm.target = targetObj.transform;
-				break;
-			case TrackType.LeftHand:
-				vrIK.solver.leftArm.target = targetObj.transform;
-				//testIKcontrol.rightHandObj = this.transform;
-				break;
-			case TrackType.RightFoot:
-				//vrIK.rightFootObj = this.transform;
-				break;
-			case TrackType.LeftFoot:
-				//vrIK.leftFootObj = this.transform;
-				break;
-			case TrackType.EyeGazePoint:
-				vrIK.solver.spine.headTarget = targetObj.transform;
-				//vrIK.lookObj = this.transform;
-				break;
-		}
+        switch (setObjectType)
+        {
+            case TrackType.RightHand:
+                vrIK.solver.rightArm.target = targetObj.transform;
+                break;
+            case TrackType.LeftHand:
+                vrIK.solver.leftArm.target = targetObj.transform;
+                //testIKcontrol.rightHandObj = this.transform;
+                break;
+            case TrackType.RightFoot:
+                //vrIK.rightFootObj = this.transform;
+                break;
+            case TrackType.LeftFoot:
+                //vrIK.leftFootObj = this.transform;
+                break;
+            case TrackType.EyeGazePoint:
+                vrIK.solver.spine.headTarget = targetObj.transform;
+                //vrIK.lookObj = this.transform;
+                break;
+        }
 
-		Debug.Log(netId +" " + gameObject.name + "Init");
-	}
+        Debug.Log(netId + " " + gameObject.name + "Init");
+    }
+
+    IEnumerator SetIKCorotuine()
+    {
+        yield return new WaitUntil(() => PlayerManager.LocalPlayer != null);
+
+        SetIKTarget();
+    }
 }
