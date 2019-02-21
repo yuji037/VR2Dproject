06a8b194b5eeafb8d3a839e309c0b9ba1cbdd2b3
umei <umeryosan@gmail.com>
2019-02-22 00:56:07 +0900

    天秤ギミック追加

---
+++ project-vr/Assets/EfR/Scripts/Gimmick/GimmickBalanceController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GimmickBalanceController : MonoBehaviour
+{
+    [SerializeField]
+    GimmickBalancer leftGimmickBalancer;
+
+    [SerializeField]
+    GimmickBalancer rightGimmickBalancer;
+
+    [SerializeField]
+    float moveYRange;
+
+    [SerializeField]
+    int maxSubCount;
+
+    [SerializeField]
+    LineRenderer lineRenderer;
+
+    [SerializeField]
+    float oneFlameMove = 0.05f;
+    float defaultY;
+
+    private void OnDrawGizmos()
+    {
+        var topLeft = leftGimmickBalancer.transform.position;
+        var topRight = rightGimmickBalancer.transform.position;
+        topLeft.y = transform.position.y + moveYRange;
+        topRight.y = transform.position.y + moveYRange;
+
+        var botLeft = leftGimmickBalancer.transform.position;
+        var botRight = rightGimmickBalancer.transform.position;
+        botLeft.y = transform.position.y - moveYRange;
+        botRight.y = transform.position.y - moveYRange;
+
+        Gizmos.DrawLine(topLeft, topRight);
+        Gizmos.DrawLine(botLeft, botRight);
+
+    }
+    // Use this for initialization
+    void Start()
+    {
+        defaultY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!leftGimmickBalancer.isServer) return;
+        var subCount = leftGimmickBalancer.RidingCount - rightGimmickBalancer.RidingCount;
+        if (subCount > maxSubCount)
+        {
+            subCount = maxSubCount;
+            Debug.Log("+補正");
+        }
+        else if (subCount < -maxSubCount)
+        {
+            subCount = -maxSubCount;
+            Debug.Log("-補正");
+        }
+
+        if (subCount !=0)
+        {
+            var toLeftPos = leftGimmickBalancer.transform.position;
+            toLeftPos.y = defaultY - moveYRange * ((float)subCount / (float)maxSubCount);
+            leftGimmickBalancer.transform.position = Vector3.Lerp(leftGimmickBalancer.transform.position, toLeftPos, oneFlameMove);
+
+            var toRightPos = rightGimmickBalancer.transform.position;
+            toRightPos.y = defaultY + moveYRange * ((float)subCount / (float)maxSubCount);
+            rightGimmickBalancer.transform.position = Vector3.Lerp(rightGimmickBalancer.transform.position, toRightPos, oneFlameMove);
+        }
+        else
+        {
+            var toLeftPos = leftGimmickBalancer.transform.position;
+            toLeftPos.y = defaultY;
+            leftGimmickBalancer.transform.position = Vector3.Lerp(leftGimmickBalancer.transform.position, toLeftPos, oneFlameMove);
+
+            var toRightPos = rightGimmickBalancer.transform.position;
+            toRightPos.y = defaultY;
+            rightGimmickBalancer.transform.position = Vector3.Lerp(rightGimmickBalancer.transform.position, toRightPos, oneFlameMove);
+
+        }
+        Debug.Log("サブカウント :"+ subCount);
+        lineRenderer.SetPosition(0, rightGimmickBalancer.transform.position);
+        lineRenderer.SetPosition(1, leftGimmickBalancer.transform.position);
+
+    }
+}
---
+++ project-vr/Assets/EfR/Scripts/Gimmick/GimmickBalancer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GimmickBalancer : GimmickBase
+{
+    [SerializeField]
+    int[] triggerIDs;
+
+    public int RidingCount { get; private set; }
+    private void Start()
+    {
+        m_acTriggerEnterAction += HitAction;
+        m_acTriggerExitAction += ExitAction;
+    }
+    void HitAction(Collider collider)
+    {
+        if (collider.isTrigger) return;
+        var gimmick = collider.GetComponent<GimmickBase>();
+        if (!gimmick) return;
+        HitAction(gimmick.GimmickID);
+    }
+    void ExitAction(Collider collider)
+    {
+        if (collider.isTrigger) return;
+        var gimmick = collider.GetComponent<GimmickBase>();
+        if (!gimmick) return;
+        ExitAction(gimmick.GimmickID);
+    }
+    void HitAction(int id)
+    {
+        foreach (var i in triggerIDs)
+        {
+            if (id == i)
+            {
+                RidingCount++;
+            }
+        }
+    }
+
+    void ExitAction(int id)
+    {
+        foreach (var i in triggerIDs)
+        {
+            if (id == i)
+            {
+                RidingCount--;
+                Debug.Log("抜けた");
+            }
+        }
+    }
+
+    //IEnumerator MoveRoutine(bool isDown)
+    //{
+    //    float multiPly = (isDown) ?-1.0f:1.0f;
+
+    //    for (float t=0;t<oneMoveDuration;t+=Time.deltaTime)
+    //    {
+    //        transform.Translate(new Vector3(0,oneMoveDuration+oneMoveValue*multiPly,0));
+    //    }
+
+    //    yield return null;
+    //}
+}
