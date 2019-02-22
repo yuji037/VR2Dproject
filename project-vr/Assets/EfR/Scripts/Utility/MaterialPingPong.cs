1b138aea75c2f63a085acb4e3af67f513b79d680
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2019-02-22 20:00:16 +0900

    ゲーム機電源ボタンのハイライトを追加

---
+++ project-vr/Assets/EfR/Scripts/Utility/MaterialPingPong.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialPingPong : MonoBehaviour {
+
+	public float min = 0f;
+	public float max = 0.5f;
+	public float speed = 1f;
+
+	Material targetMaterial;
+
+	// Use this for initialization
+	void Start () {
+		targetMaterial = GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		targetMaterial.SetFloat("_Outline", Mathf.PingPong(Time.time * speed, max - min) + min);
+	}
+}
