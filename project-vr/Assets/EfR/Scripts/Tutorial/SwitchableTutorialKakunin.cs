83fdc54ce845aacc4d20dd5e7614e3ea40a4865c
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2019-02-27 17:38:39 +0900

    初回プレイか確認するウィンドウ追加

---
+++ project-vr/Assets/EfR/Scripts/Tutorial/SwitchableTutorialKakunin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchableTutorialKakunin : MonoBehaviour, ISwitchableObject
+{
+    [SerializeField]
+    bool yes = true;
+
+    [SerializeField]
+    public TutorialKakunin kakuninPanel;
+
+    public void OnAction()
+    {
+        if (yes)
+        {
+            TVSwitch.IsOn = true;
+            GameCoordinator.GetInstance().ChangeStage("tutorial");
+        }
+        kakuninPanel.SetActiveThisObj(false);
+    }
+
+    public void OffAction()
+    {
+
+    }
+}
---
+++ project-vr/Assets/EfR/Scripts/Tutorial/TutorialKakunin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialKakunin : MonoBehaviour {
+    
+    public void SetActiveThisObj(bool isActive)
+    {
+        SetActiveObj(gameObject, isActive);
+    }
+
+    void SetActiveObj(GameObject obj, bool isActive)
+    {
+        var rens = obj.GetComponentsInChildren<Renderer>();
+        foreach (var ren in rens)
+            ren.enabled = isActive;
+
+        var cols = obj.GetComponentsInChildren<Collider>();
+        foreach (var col in cols)
+            col.enabled = isActive;
+    }
+}
