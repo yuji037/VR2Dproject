a1c6042f18509c6d83cd5960c05fe15eee4c17e0
umei <umeryosan@gmail.com>
2019-03-01 14:46:49 +0900

    張り付きオブジェクトを修正

--- project-vr/Assets/EfR/Scripts/Character/FloorStickableObject.cs
+++ project-vr/Assets/EfR/Scripts/Character/FloorStickableObject.cs
@@ -2,74 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-public class FloorStickableObject : MonoBehaviour
-{
+public class FloorStickableObject : MonoBehaviour {
     Vector3 halfExtents;
     Vector3 prevPos;
     NetworkIdentity netId;
 
     Transform defaultParent;
-
-    Rigidbody m_RigidBody;
-
-    Transform stickTarget;
-    Vector3 preTargetPos;
     // Use this for initialization
-    void Start()
-    {
+	void Start () {
         halfExtents = transform.localScale * 0.49f;
         defaultParent = transform.parent;
         netId = GetComponent<NetworkIdentity>();
-        m_RigidBody = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
+	
+	// Update is called once per frame
+	void Update () {
         if (netId && !netId.hasAuthority) return;
-        var origin = transform.position;
-        origin.y -= halfExtents.y*0.52f;
+
         RaycastHit hit;
-        if (Physics.Raycast(
-            transform.position + new Vector3(0, 0.1f, 0),
-            Vector3.down,
-            out hit,
-            halfExtents.y)&&
-            hit.collider.gameObject.tag == "LaserPointerFloorCreate")
+        if (Physics.BoxCast(transform.position, halfExtents, Vector3.down,out hit, transform.rotation, 0.1f)&&
+            hit.collider.gameObject.tag=="LaserPointerFloorCreate")
         {
-            Debug.Log(hit.transform);
-            stickTarget = hit.transform;
-            preTargetPos = stickTarget.transform.position;
+            transform.parent = hit.transform;
         }
         else
         {
-            stickTarget = null;
-        }
-        if (!IsRigidBodyMove()&&
-            stickTarget)
-        {
-            var move = stickTarget.transform.position - preTargetPos;
-            transform.Translate(move,Space.World);
-            preTargetPos = stickTarget.transform.position;
-            Debug.Log("TR"+move);
-        }
-    }
-
-    bool IsRigidBodyMove()
-    {
-        return m_RigidBody &&
-            !m_RigidBody.isKinematic;
-    }
-
-    private void FixedUpdate()
-    {
-        if (IsRigidBodyMove()&&
-            stickTarget)
-        {
-            var move=stickTarget.transform.position-preTargetPos;
-            m_RigidBody.MovePosition(transform.position+move);
-            preTargetPos = stickTarget.transform.position;
-            Debug.Log("RI"+move);
+            transform.parent = defaultParent;
         }
     }
 }
