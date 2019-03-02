3a53160eeaeb056e251b5ce7edf67ff0a0d98665
yuji037 <pnst_n_th_wr037@yahoo.co.jp>
2019-03-02 16:42:01 +0900

    チュートリアル修正

--- project-vr/Assets/EfR/Scripts/Utility/MaterialPingPong.cs
+++ project-vr/Assets/EfR/Scripts/Utility/MaterialPingPong.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class MaterialPingPong : MonoBehaviour {
 
+	public enum ParamType {
+		OutlineWidth,
+		ColorAlpha,
+	}
+
+	
+	public ParamType changeParamType = ParamType.OutlineWidth;
+
 	public float min = 0f;
 	public float max = 0.5f;
 	public float speed = 1f;
@@ -17,6 +25,17 @@ public class MaterialPingPong : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		targetMaterial.SetFloat("_Outline", Mathf.PingPong(Time.time * speed, max - min) + min);
+		var value = Mathf.PingPong(Time.time * speed, max - min) + min;
+
+		switch ( changeParamType )
+		{
+			case ParamType.OutlineWidth:
+				targetMaterial.SetFloat("_Outline", value);
+				break;
+			case ParamType.ColorAlpha:
+				var c = targetMaterial.color;
+				targetMaterial.color = new Color(c.r, c.g, c.b, value);
+				break;
+		}
 	}
 }
